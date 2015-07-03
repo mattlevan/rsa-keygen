87239948521d0d471cbe98c29723585802166ef2
Matt Levan <odds@matt-laptop.local>
2015-07-03 16:44:52 -0700

    Completed number generation

--- rsa-keygen.c
+++ rsa-keygen.c
@@ -34,44 +34,58 @@
 #include <math.h>
 #include <primesieve.h>
 
+uint64_t gcd(uint64_t a, uint64_t b);
+
 int main()
 {
     uint64_t nth, random, p, q, d, e, n; 
     uint64_t d_x_e, phi_n;
 
-    // Generate three random, large primes p and q
-    for (int i = 0; i < 3; i++) {
+    // Generate two random, large primes p and q
+    for (int i = 0; i < 2; i++) {
         // Fetch a random number using arc4random
-        nth = arc4random_uniform(UINT16_MAX);
+        nth = arc4random_uniform(UINT8_MAX);
 
         if (i == 0) {
             // Find the nth prime from 0
             p = primesieve_nth_prime(nth, 0);
-            printf("p      = %llu\n", p);
+            printf("p       = %llu\n", p);
         }
-        else if (i == 1) {
+        else {
             // Find the nth prime from 0
             q = primesieve_nth_prime(nth, 0);
-            printf("q      = %llu\n", q);
-        }
-        else if (i == 2) {
-            // Find the nth prime from 0
-            e = primesieve_nth_prime(nth, 0);
-            printf("e      = %llu\n", e);
+            printf("q       = %llu\n", q);
         }
     }
 
     // Calculate and print the value of n
     n = p*q;
-    printf("n      = %llu\n", n);
+    printf("n       = %llu\n", n);
 
     // Calculate and print the value of phi(n)
     phi_n = (p-1)*(q-1);
-    printf("phi(n) = %llu\n", phi_n);
+    printf("phi(n)  = %llu\n", phi_n);
+
+    // Generate small number e that is relatively prime with phi(n)
+    for (e = arc4random_uniform(11); e > 0; e++) {
+        if (gcd(e, phi_n) == 1) {
+            printf("e       = %llu\n", e);
+            break;
+        }
+    }
 
     // Calculate and print the value of d
+    d = (phi_n + 1) / e;
+    printf("d       = %llu\n", d);
+    printf("(d*e)%cn = %llu\n", '%', (d*e)%n);
 
-    printf("d      = %llu\n\n", d);
-    
     return EXIT_SUCCESS;
+}
+
+uint64_t gcd(uint64_t a, uint64_t b) {
+    if (b == 0) {
+        return a;
+    }
+
+    return gcd(b, a % b);
 }
